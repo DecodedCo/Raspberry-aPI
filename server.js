16a17e8465b180baa6936bd407538aa02d2ea5ec
Joe Dytrych <me@jdytrych.co.uk>
2013-10-04 16:42:37 +0100

    Use a more functional style of programming

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.set('jsonp callback name', 'callback');
 function errorHandler(res, err) {
   'use strict';
 
+  console.log('Error:', err);
+
   res.send("<h1>Oops</h1><p>Sorry, there seems to be a problem!</p>\n\n<!--\n\n" + err.stack + "\n\n-->");
   throw err;
 }
@@ -33,105 +35,137 @@ function errorHandler(res, err) {
   http://api.decoded.co/checkin/NAME returns all records
   http://api.decoded.co/checkin/NAME?username=twitterhandle stores checkin and returns number of checkins
 */
-
-app.all('/checkin*', function (req, res) {
+function setupCheckins(app) {
   'use strict';
 
-  // parse the URL request
-  var reqUrl = url.parse(req.url, true),
-    query = reqUrl.query,
-  // strip out non-word characters \W and remove initial checkin
-    dbName = reqUrl.pathname.replace(/\W/g, '').replace(/^checkin/, ''),
-    db;
+  // Get the db name from the url path. Strip non-word characters and 'checkin/' from the start
+  function getDbName(url) {
+    var dbName = url.pathname.replace(/\W/g, '').replace(/^checkin/, '');
 
-  if (dbName) {
-    dbName += '.db';
-  } else {
-    dbName = 'default.db';
+    if (dbName) {
+      return './data/' + dbName + '.db';
+    }
+
+    return './data/default.db';
   }
 
-  db = store.new('./checkin/' + dbName, function () {
-    if (query.username) {
-      // User specific stuff.
-      var username = query.username.replace(/\W/g, '').toLowerCase();
+  // Save the checkins into the user's document in db, the send the result to res
+  function saveAndSendCheckin(user, checkIns, db, res) {
+    db.save(user, checkIns, function (err) {
+      if (err) {
+        errorHandler(res, err);
+        return;
+      }
 
-      db.get(username, function (err, checkIns) {
-        if (err) {
-          // Username doesn't exist, create a document for the user
-          db.save(username, 1, function (err) {
-            if (err) {
-              errorHandler(res, err);
-              return;
-            }
+      try {
+        res.jsonp({
+          username: user,
+          checkIns: checkIns
+        });
+      } catch (e) {
+        errorHandler(res, err);
+      }
+    });
+  }
 
-            res.jsonp({
-              username: username,
-              checkIns: 1
-            });
-          });
-        } else {
-          db.save(username, checkIns + 1, function (err) {
-            if (err) {
-              errorHandler(res, err);
-              return;
-            }
+  // Send everything from db to res by jsonp
+  function sendAll(db, res) {
+    db.all(function (err, results) {
+      if (err) {
+        return errorHandler(res, err);
+      }
 
-            res.jsonp({
-              username: username,
-              checkIns: checkIns + 1
-            });
-          });
-        }
-      });
-    } else {
-      // Send all results
-      db.all(function (err, results) {
-        if (err) {
-          return errorHandler(res, err);
-        }
-
+      try {
         res.jsonp(results);
-      });
-    }
-  });
-});
+      } catch (e) {
+        errorHandler(res, e);
+      }
+    });
+  }
+
+  // When a checkin request comes in,
+  function checkinListener(req, res) {
+    var reqUrl = url.parse(req.url, true),
+      dbName = getDbName(reqUrl),
+      db;
+
+    db = store.new(dbName, function () {
+      if (reqUrl.query.username) {
+        var username = reqUrl.query.username.replace(/\W/g, '').toLowerCase();
+
+        // Try to fetch the user's checkins. If there's an error, that user doesn't exist.
+        db.get(username, function (err, checkIns) {
+          if (err) {
+            // user doesn't exist, create it
+            saveAndSendCheckin(username, 1, db, res);
+          } else {
+            // user exists, increase their checkins
+            saveAndSendCheckin(username, checkIns + 1, db, res);
+          }
+        });
+      } else {
+        sendAll(db, res);
+      }
+    });
+  }
+
+  app.all('/checkin*', checkinListener);
+}
 
 /*
   Google spreadsheet CSV to JSON
   Data Visualisation in a Day
 */
-app.all('/cleanGoogle*', function (req, res) {
-  'use strict';
 
-  // Parse the URL for a key
-  var reqUrl = url.parse(req.url, true),
-    key = reqUrl.pathname.replace(/\W/g, '').replace(/^cleanGoogle/, '');
-
-  if (key.length === 0) {
-    res.send(404, "No key specified.");
-    return;
+function setupCleanGoogle(app) {
+  // Pull the key out from the URL
+  function getKey(reqUrl) {
+    return url.parse(reqUrl, true).pathname.replace(/\W/g, '').replace(/^cleanGoogle/, '');
   }
 
-  // Grab the document from Google Docs
-  request('https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&single=true&gid=0&output=csv&key=' + key, function (error, response, body) {
+  // Get a URL for the key
+  function getURL(key) {
+    return 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&single=true&gid=0&output=csv&key=' + key;
+  }
 
-    if (!error && response.statusCode === 200) {
-      // convert csv to JSON
-      csv().from(body, {
-        columns: true
-      }).to.array(function (data) {
-        res.jsonp(data);
-      });
-    } else {
-      res.send(response.statusCode, body);
+  function getData(key, res, req, callback) {
+    request(getURL(key), function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        // csv -> json:
+        csv().from(body, {columns: true}).to.array(callback);
+      } else {
+        res.send(response.statusCode, body);
+      }
+
+      // Log the request
+      console.log(url + ', ' + response.statusCode + ', ' + req.ip + ', ' + req.headers['user-agent']);
+    });
+  }
+
+  function cleanGoogleListener(req, res) {
+    // Parse the URL for a key
+    var key = getKey(req.url);
+
+    if (key.length === 0) {
+      res.send(404, "No key specified.");
+      return;
     }
 
-    // log the request
-    console.log(key + ',' + response.statusCode + ',' + req.ip + ',' + req.headers['user-agent']);
+    // Grab the document from Google Docs
+    getData(key, res, req, function (data) {
+      try {
+        res.jsonp(data);
+      } catch(e) {
+        errorHandler(res, e);
+      }
+    });
+  }
 
-  });
-});
+  app.all('/cleanGoogle*', cleanGoogleListener);
+}
 
+setupCheckins(app);
+setupCleanGoogle(app);
 
 // Default behavior
 app.all('*', function (req, res) {
